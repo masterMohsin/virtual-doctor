c0d70d2617369e0b1d296462ed31d75cee6ed770
masterMohsin <itzmohsinmunir1011@gmail.com>
2025-07-23 13:35:55 +0500

    this is first change

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -8,11 +8,11 @@ const Layout = () => {
     <>
       <div className="flex min-h-screen">
       {/* Sidebar */}
-      <div className="md:w-72 min-h-screen fixed left-0 top-0 bottom-24 bg-white z-10">
+      <div className="md:w-56  min-h-screen   left-0 top-0 bottom-24  ">
         <Sidebar />
       </div>
       {/* Main Content */}
-      <div className="flex-1 ml-72 flex flex-col min-h-screen overflow-auto">
+      <div className="flex-1 w-full  flex  flex-col min-h-screen">
         <main className="flex-1">
           <Outlet />
         </main>
